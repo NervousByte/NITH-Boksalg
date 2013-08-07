0c2f7f7ca3075088bcb993ac9fc34e5049e5af3a
NervousByte <NervousByte@gmail.com>
2013-08-07 13:59:20 +0200

    First WebPage

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php $head = ""; ?>
+<?php include("masterStart.php"); ?>
+<br/><br/>
+	<div style = "text-align: center;">
+		<a href = "SelectStudy.php">
+			<img src = "img/forside.jpg" alt = "Student til høsten?">
+		</a>
+	</div>
+	<div style = "margin-left: 100px; font-size: large">
+	<br/><br/>
+	Mer tekst kommer her...
+	</div>
+	
+<?php include("masterEnd.php"); ?>
