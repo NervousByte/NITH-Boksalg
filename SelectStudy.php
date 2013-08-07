5f86e4a7a94d9a79e4b6c4a3a98ae529d7cfc0b9
NervousByte <NervousByte@gmail.com>
2013-08-07 17:42:47 +0200

    tmp.php and SelectStudy.php

---
+++ SelectStudy.php
@@ -0,0 +1,142 @@
+<?php $head = 
+".study { display: inline-block; width: 350px; margin: 20px 0px 0px 60px; }
+.study div { display: inline-block; }
+.study a { display: inline-block; }
+h1 {margin-top : 0px;}";  ?>
+<?php include("masterStart.php"); ?>
+<div style = "text-align: center; display : inline;">
+<br/>
+<h1>Velg hvilken studierettning du vil ha bøker for</h1>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=spillprog">
+		<img width = 200 height = 200; src = "img/spillprog.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=spillprog">
+		<p>Spillprogrammering</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=prog">
+		<img width = 200 height = 200; src = "img/prog.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=prog">
+		<p>Programmering</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=app">
+		<img width = 200 height = 200; src = "img/app.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=app">
+		<p>Apputvikkling</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=spilldesign">
+		<img width = 200 height = 200; src = "img/spilldesign.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=spilldesign">
+		<p>Spill Design</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=3d">
+		<img width = 200 height = 200; src = "img/3d.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=3d">
+		<p>3D Grafikk</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=interaktiv">
+		<img width = 200 height = 200; src = "img/interaktiv.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=interaktiv">
+		<p>Inteeraktivt Design</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=markedsforing">
+		<img width = 200 height = 200; src = "img/markedsforing.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=markedsforing">
+		<p>Digital Markedsføring</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=ebusiness">
+		<img width = 200 height = 200; src = "img/ebusiness.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=ebusiness">
+		<p>E-Business</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php?tag=industri">
+		<img width = 200 height = 200; src = "img/industri.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php?tag=industri">
+		<p>Industribachelor</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<div class = "study">
+	<a href = "Books.php">
+		<img width = 200 height = 200; src = "img/alle.jpg">
+	</a>
+	
+	<div>
+	<a href = "Books.php">
+		<p>Alle</p>
+	</a>
+	<br/><br/><br/><br/>
+	</div>
+</div>
+
+<?php include("masterEnd.php"); ?>
---
+++ tmp.php
@@ -0,0 +1,3 @@
+<?php $head = "#Content { background-image: url('img/PageUnderConstruction.png'); background-repeat:no-repeat; background-size: 100% 100%; }"; ?>
+<?php include("masterStart.php"); ?>
+<?php include("masterEnd.php"); ?>
